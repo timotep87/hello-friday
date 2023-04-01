0529bb3aee4465024c34d9d92df92f010d40625e
Timót Mechler <timot.mechler@tui.nl>
2023-04-01 18:42:00 +0200

    fixing typo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, but it's Saterday!!!
+          Hello, but it's Saturday!!!
         </p>
         <a
           className="App-link"
